59e6f36bcb08fc3d1839437b8d22895087b824fa
Patty <L224749391@gmail.com>
2022-05-21 20:29:44 +0800

    search by description

---
+++ DramaEngine/views/search_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 16:03:31 2022
+
+@author: linxiangling
+"""
+
+import requests 
+import json
+import flask
+from flask import request, Blueprint, jsonify
+from modesl import search
+
+search_api=Blueprint('search_api', __name__)
+
+@search_api.route('search_by_description', methods=['post'])
+def search_by_description():
+    data = request.get_json()
+    tv_or_movie = data['tv_or_movie']
+    genres_ids = data['genres_ids']
+    user_description = data['user_description']
+    result = search.search_by_description_model(tv_or_movie, genres_ids, user_description)
+    return jsonify({"result":result})
