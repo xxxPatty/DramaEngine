89f16e00e2f638377e70031ed8bcc17e4a514f52
Patty <L224749391@gmail.com>
2022-05-22 21:49:04 +0800

    add search by description api comment

--- DramaEngine/views/search_api.py
+++ DramaEngine/views/search_api.py
@@ -17,8 +17,8 @@ search_api=Blueprint('search_api', __name__)
 @search_api.route('search_by_description', methods=['post'])
 def search_by_description():
     data = request.get_json()
-    tv_or_movie = data['tv_or_movie']
-    genres_ids = data['genres_ids']
-    user_description = data['user_description']
+    tv_or_movie = data['tv_or_movie']   #tv(0)還是movie(1)
+    genres_ids = data['genres_ids']     #類型list
+    user_description = data['user_description']     #描述string
     result = search.search_by_description_model(tv_or_movie, genres_ids, user_description)
     return jsonify({"result":result})
