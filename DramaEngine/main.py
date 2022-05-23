6ab38c6f41d45ea5fdbd25792db5e20b75997f30
Patty <L224749391@gmail.com>
2022-05-24 00:02:12 +0800

    Update main.py

--- DramaEngine/main.py
+++ DramaEngine/main.py
@@ -8,13 +8,27 @@ Created on Sat May 21 15:52:43 2022
 
 from flask import Flask
 from flask import render_template
+from views import register_blueprint
 
-app = Flask(__name__)
+from flask import Flask
+from flask_cors import CORS
+import models
+from views import register_blueprint
+
+
 # @app.route("/")
 # def hello():
 #     return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
+    app.jinja_env.auto_reload = True
+    CORS(app)
+    # register app
+    register_blueprint(app)
+    return app
     
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0")
 
     
