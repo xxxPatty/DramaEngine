7f31265e25ca167456f0050a0c537875429a7feb
cih <angel08012000@gmail.com>
2022-05-24 22:20:03 +0800

    新增: get_all_gener API

--- DramaEngine/views/__init__.py
+++ DramaEngine/views/__init__.py
@@ -8,8 +8,9 @@ Created on Sat May 21 16:05:37 2022
 
 from .index_web import index_web
 from .search_api import search_api
+from .pre_process_api import pre_process_api
 
-blueprint_prefix = [(search_api, ""), (index_web, "")]
+blueprint_prefix = [(search_api, ""), (index_web, ""), (pre_process_api, "")]
 
 def register_blueprint(app):
     for blueprint, prefix in blueprint_prefix:
