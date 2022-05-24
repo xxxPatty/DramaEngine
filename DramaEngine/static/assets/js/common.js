96f63a1b71a5e3341bb81e07a8de0f8f8ae2535c
cih <angel08012000@gmail.com>
2022-05-25 02:04:23 +0800

    新增: 串接 search API
    串接資料結束～ 差排版的部分

--- DramaEngine/static/assets/js/common.js
+++ DramaEngine/static/assets/js/common.js
@@ -1,5 +1,6 @@
 var prefix = "http://192.168.0.171:8082/";
 var geners = "get_all_gener";
+var search_by_des = "search_by_description";
 
 var buttonArr;
 
@@ -41,6 +42,49 @@ function clickButton(id){
     
     console.log(`目前的陣列: `);
     console.log(buttonArr);
+}
+
+function search(){
+    console.log("開始搜尋");
+    var url = prefix + search_by_des;
+    var description = document.getElementById("Description").value;
+
+    var request = {
+        "tv_or_movie": 1,
+        "genres_ids": buttonArr,
+        "user_description": description
+    };
+    console.log("送出去的request: ");
+    console.log(request);
+
+    // 開始搜尋
+    $.ajax({
+        url: url,
+        data: JSON.stringify(request),
+        type: "POST",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(response){
+            console.log("搜尋成功，結果為...");
+            console.log(response);
+            var result = response.result;
+
+            var moviesHtml = "";
+            for(var i=0; i<result.length; i++){
+                moviesHtml += `<div>${result[i].title}__</div><br>`;
+                console.log(`電影結果: ${result[i].title}`);
+            }
+
+            if(result.length==0){
+                moviesHtml = "無結果";
+            }
+
+            document.getElementById("sever_output").innerHTML = moviesHtml;
+        },
+        error: function(){
+            console.log("error");
+        }
+    });
 }
 
 function start(){
